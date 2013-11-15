33e92cf35530fb2ba5ce32a325b32efdf010a8f2
LarsOle <larsole2000@gmail.com>
2013-11-15 14:58:06 +0100

    Update RSSFeed.py

--- RSSFeed.py
+++ RSSFeed.py
@@ -1,19 +1,19 @@
-  import urllib.request
-  import xml.dom.minidom as dom
-  import re
+import urllib.request
+import xml.dom.minidom as dom
+import re
    
-  class RSSFeed:
+class RSSFeed:
    
-  	def __init__(self, url, parent=None):
-  		super(RSSFeed, self).__init__()
+	def __init__(self, url, parent=None):
+		super(RSSFeed, self).__init__()
   
- 		self.resp = urllib.request.urlopen(url)
- 		self.data = self.resp.read()
- 		self.dataEncoded = self.data.decode('UTF-8')
+		self.resp = urllib.request.urlopen(url)
+		self.data = self.resp.read()
+		self.dataEncoded = self.data.decode('UTF-8')
 		self.xmlFile = dom.parseString(self.dataEncoded)
   
- 		def searchTag(self, seriesName, tag):
- 		self.tag = tag
+	def searchTag(self, seriesName, tag):
+		self.tag = tag
   
 		for node in self.xmlFile.getElementsByTagName(self.tag):
  			description = node.getElementsByTagName('title')
