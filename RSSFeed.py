a5ef3e58a3aaf4cc846824bc9783fa6b4fb003bb
LarsOle <larsole2000@gmail.com>
2013-11-15 14:09:06 +0100

    Create RSSFeed.py

---
+++ RSSFeed.py
@@ -0,0 +1,27 @@
+  import urllib.request
+  import xml.dom.minidom as dom
+  import re
+   
+  class RSSFeed:
+   
+  	def __init__(self, url, parent=None):
+  		super(RSSFeed, self).__init__()
+  
+ 		self.resp = urllib.request.urlopen(url)
+ 		self.data = self.resp.read()
+ 		self.dataEncoded = self.data.decode('UTF-8')
+		self.xmlFile = dom.parseString(self.dataEncoded)
+  
+ 		def searchTag(self, seriesName, tag):
+ 		self.tag = tag
+  
+		for node in self.xmlFile.getElementsByTagName(self.tag):
+ 		description = node.getElementsByTagName('title')
+ 		if seriesName.lower() in description[0].toxml().lower():
+ 			print(seriesName)
+ 			print(self.getEpisode(description[0].toxml()))
+                           
+	def getEpisode(self, title):
+		self.title = title
+		matches = re.search("S(\d+)E(\d+)", self.title, re.I).groups()
+		return matches    
